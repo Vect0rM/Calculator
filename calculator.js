93ce52b3699070660c5bde8e1707d7b353239016
Misha <m.skvortsovvv@gmail.com>
2022-01-23 03:55:22 +0300

    3

--- calculator.js
+++ calculator.js
@@ -15,7 +15,104 @@ function clear() {
     sout.textContent = "0";
 }
 
+function cc(e) {
+    let key = e.key;
+    if (key === "Escape") {
+        clear();
+    }
+    if (key === "Backspace") {
+        back();
+    }
+
+    function input() {
+        if (digit.includes(key)) {
+            if (num2 === "" && sign === "") {
+                num1 += key;
+                sout.textContent = num1;
+            }
+            else if (num1 !== "" && num2 !== "" && finish) {
+                num2 = key;
+                finish = false;
+                sout.textContent = num2;
+            } else {
+                num2 += key;
+                sout.textContent = num2;
+            }
+            return;
+        }
+    }
+    if (digit.includes(key)) {
+        if (num2 === "" && sign === "") {
+            num1 += key;
+            sout.textContent = num1;
+        }
+        else if (num1 !== "" && num2 !== "" && finish) {
+            num2 = key;
+            finish = false;
+            sout.textContent = num2;
+        } else {
+            num2 += key;
+            sout.textContent = num2;
+        }
+        return;
+    }
+    if (action.includes(key)) {
+        sign = key;
+        sout.textContent = sign;
+    }
+    if (key === "=") {
+        switch (sign){
+            case "+":
+                num1 = (+num1) + (+num2);
+                num2 = "";
+                break;
+            case "-":
+                num1 = num1 - num2;
+                num2 = "";
+                break;
+            case "X":
+                num1 = num1 * num2;
+                num2 = "";
+                break;
+            case "/":
+                if (num2 === 0) {
+                    sout.textContent = "Ошибка";
+                    num1 = "";
+                    num2 = "";
+                    sign = "";
+                    return;
+                }
+                num1 = num1 / num2;
+                num2 = "";
+                break;
+        }
+        finish = true;
+        sout.textContent = num1;
+    }
+    function back() {
+        if (num2 === "") {
+            num1 = Math.floor(num1 / 10);
+            sout.textContent = num1;
+        }
+        else if (num1 !== "" && num2 !== "" && finish) {
+            num2 = key;
+            finish = false;
+            sout.textContent = num2;
+        }
+        else {
+            num2 = Math.floor(num2 / 10);
+            sout.textContent = num2;
+        }
+    }
+
+    if (key === "bc") {
+        back();
+    }
+}
+
 document.querySelector(".ac").onclick = clear();
+document.addEventListener("keypress", cc)
+
 document.querySelector(".buttons").onclick = (event) => {
     if (!event.target.classList.contains("btn")) {
         return;
@@ -23,9 +120,10 @@ document.querySelector(".buttons").onclick = (event) => {
     if (event.target.classList.contains("ac")) {
         clear();
     }
-    sout.textContent = "";
-    const key = event.target.textContent;
 
+    sout.textContent = "";
+
+    let key = event.target.textContent;
 
     if (digit.includes(key)) {
         if (num2 === "" && sign === "") {
